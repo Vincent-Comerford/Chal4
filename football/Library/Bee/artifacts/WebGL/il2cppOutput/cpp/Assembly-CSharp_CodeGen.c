b79e6e7002ce71b2332cb240dd1e798f4632631d
Vincent-Comerford <b00140980@mytudublin.ie>
2022-11-13 18:01:59 +0000

    chal4
    chal 4

---
+++ football/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,133 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void EnemyX::Start()
+extern void EnemyX_Start_m344B6AABB07BCD8BD45B6365348CEE876A76EB3A (void);
+// 0x00000002 System.Void EnemyX::Update()
+extern void EnemyX_Update_mE2F7221B3009D66CF995F2AC41ABBB285B328290 (void);
+// 0x00000003 System.Void EnemyX::OnCollisionEnter(UnityEngine.Collision)
+extern void EnemyX_OnCollisionEnter_mEAAF0CACB97A3F477582034B3EAD401F8D30C94A (void);
+// 0x00000004 System.Void EnemyX::.ctor()
+extern void EnemyX__ctor_mB20BA4CA51958C20D82960C349538202EB01DECA (void);
+// 0x00000005 System.Void PlayerControllerX::Start()
+extern void PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606 (void);
+// 0x00000006 System.Void PlayerControllerX::Update()
+extern void PlayerControllerX_Update_mE690DFA5B4D44A839AC48EC903E382790B5CCC8B (void);
+// 0x00000007 System.Void PlayerControllerX::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerControllerX_OnTriggerEnter_m3CF3B3203B081E6F6C5371B913DCD5FCBD1C0780 (void);
+// 0x00000008 System.Collections.IEnumerator PlayerControllerX::PowerupCooldown()
+extern void PlayerControllerX_PowerupCooldown_mCA4B4C0BE6E27C8DFA5DE4E95737DCA5DE3DA7BC (void);
+// 0x00000009 System.Void PlayerControllerX::OnCollisionEnter(UnityEngine.Collision)
+extern void PlayerControllerX_OnCollisionEnter_m35E6B0ED27CD27A2652507073E9CD8965D19E32D (void);
+// 0x0000000A System.Void PlayerControllerX::.ctor()
+extern void PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A (void);
+// 0x0000000B System.Void PlayerControllerX/<PowerupCooldown>d__11::.ctor(System.Int32)
+extern void U3CPowerupCooldownU3Ed__11__ctor_m40BC1F33A6C4231A41E92AA47B9FC4AD75971AB3 (void);
+// 0x0000000C System.Void PlayerControllerX/<PowerupCooldown>d__11::System.IDisposable.Dispose()
+extern void U3CPowerupCooldownU3Ed__11_System_IDisposable_Dispose_mF3AE6E99AF1067204D51227CF1B831102EB901B2 (void);
+// 0x0000000D System.Boolean PlayerControllerX/<PowerupCooldown>d__11::MoveNext()
+extern void U3CPowerupCooldownU3Ed__11_MoveNext_mB4C027B1C5F973D9065FAAEEE0E15A4E06C8D665 (void);
+// 0x0000000E System.Object PlayerControllerX/<PowerupCooldown>d__11::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CPowerupCooldownU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA93BCCE01952B4D579D82EC6D06E9FA6177D12FE (void);
+// 0x0000000F System.Void PlayerControllerX/<PowerupCooldown>d__11::System.Collections.IEnumerator.Reset()
+extern void U3CPowerupCooldownU3Ed__11_System_Collections_IEnumerator_Reset_mA481AE2157AB6DFC2CE072E96BAD52751C6CA36F (void);
+// 0x00000010 System.Object PlayerControllerX/<PowerupCooldown>d__11::System.Collections.IEnumerator.get_Current()
+extern void U3CPowerupCooldownU3Ed__11_System_Collections_IEnumerator_get_Current_m5DD80D18C11BECB7ACD84981E2C901ABC93B651E (void);
+// 0x00000011 System.Void RotateCameraX::Update()
+extern void RotateCameraX_Update_mDEB2FEBD64480EB28EAE47E3A680F37F79F0AF09 (void);
+// 0x00000012 System.Void RotateCameraX::.ctor()
+extern void RotateCameraX__ctor_m370F5E3DC01EC0C36A8CCD1C8151B1FD76536AD3 (void);
+// 0x00000013 System.Void SpawnManagerX::Update()
+extern void SpawnManagerX_Update_mCB247E2DB1AD8C98E5B2E329FC986FCC0F204A65 (void);
+// 0x00000014 UnityEngine.Vector3 SpawnManagerX::GenerateSpawnPosition()
+extern void SpawnManagerX_GenerateSpawnPosition_m34B2D95CDA89A9C79434E6F2B2EF41D3BBDE1FB7 (void);
+// 0x00000015 System.Void SpawnManagerX::SpawnEnemyWave(System.Int32)
+extern void SpawnManagerX_SpawnEnemyWave_mDD0349D87F15A63A4C43AF964086566F26B40097 (void);
+// 0x00000016 System.Void SpawnManagerX::ResetPlayerPosition()
+extern void SpawnManagerX_ResetPlayerPosition_m9AA2D8260FA13947D7C27217C2B9E19D173840B2 (void);
+// 0x00000017 System.Void SpawnManagerX::.ctor()
+extern void SpawnManagerX__ctor_m967D2E81B63EF3B6BCA2C206459FFAB7EBFFE60E (void);
+static Il2CppMethodPointer s_methodPointers[23] = 
+{
+	EnemyX_Start_m344B6AABB07BCD8BD45B6365348CEE876A76EB3A,
+	EnemyX_Update_mE2F7221B3009D66CF995F2AC41ABBB285B328290,
+	EnemyX_OnCollisionEnter_mEAAF0CACB97A3F477582034B3EAD401F8D30C94A,
+	EnemyX__ctor_mB20BA4CA51958C20D82960C349538202EB01DECA,
+	PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606,
+	PlayerControllerX_Update_mE690DFA5B4D44A839AC48EC903E382790B5CCC8B,
+	PlayerControllerX_OnTriggerEnter_m3CF3B3203B081E6F6C5371B913DCD5FCBD1C0780,
+	PlayerControllerX_PowerupCooldown_mCA4B4C0BE6E27C8DFA5DE4E95737DCA5DE3DA7BC,
+	PlayerControllerX_OnCollisionEnter_m35E6B0ED27CD27A2652507073E9CD8965D19E32D,
+	PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A,
+	U3CPowerupCooldownU3Ed__11__ctor_m40BC1F33A6C4231A41E92AA47B9FC4AD75971AB3,
+	U3CPowerupCooldownU3Ed__11_System_IDisposable_Dispose_mF3AE6E99AF1067204D51227CF1B831102EB901B2,
+	U3CPowerupCooldownU3Ed__11_MoveNext_mB4C027B1C5F973D9065FAAEEE0E15A4E06C8D665,
+	U3CPowerupCooldownU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA93BCCE01952B4D579D82EC6D06E9FA6177D12FE,
+	U3CPowerupCooldownU3Ed__11_System_Collections_IEnumerator_Reset_mA481AE2157AB6DFC2CE072E96BAD52751C6CA36F,
+	U3CPowerupCooldownU3Ed__11_System_Collections_IEnumerator_get_Current_m5DD80D18C11BECB7ACD84981E2C901ABC93B651E,
+	RotateCameraX_Update_mDEB2FEBD64480EB28EAE47E3A680F37F79F0AF09,
+	RotateCameraX__ctor_m370F5E3DC01EC0C36A8CCD1C8151B1FD76536AD3,
+	SpawnManagerX_Update_mCB247E2DB1AD8C98E5B2E329FC986FCC0F204A65,
+	SpawnManagerX_GenerateSpawnPosition_m34B2D95CDA89A9C79434E6F2B2EF41D3BBDE1FB7,
+	SpawnManagerX_SpawnEnemyWave_mDD0349D87F15A63A4C43AF964086566F26B40097,
+	SpawnManagerX_ResetPlayerPosition_m9AA2D8260FA13947D7C27217C2B9E19D173840B2,
+	SpawnManagerX__ctor_m967D2E81B63EF3B6BCA2C206459FFAB7EBFFE60E,
+};
+static const int32_t s_InvokerIndices[23] = 
+{
+	988,
+	988,
+	859,
+	988,
+	988,
+	988,
+	859,
+	963,
+	859,
+	988,
+	851,
+	988,
+	940,
+	963,
+	988,
+	963,
+	988,
+	988,
+	988,
+	987,
+	851,
+	988,
+	988,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	23,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
